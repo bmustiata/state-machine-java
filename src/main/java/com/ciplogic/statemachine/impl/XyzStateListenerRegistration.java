4ccbd41530f7750c48987292ab31620f8bf50b02
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2017-04-21 09:43:06 +0200

    Removed xyz references.

--- src/main/java/com/ciplogic/statemachine/impl/XyzStateListenerRegistration.java
+++ src/main/java/com/ciplogic/statemachine/impl/XyzStateListenerRegistration.java
@@ -7,19 +7,19 @@ import java.util.Set;
 import java.util.function.Consumer;
 
 public class XyzStateListenerRegistration<T> {
-    private XyzStateListeners<T> xyzStateListeners;
+    private XyzStateListeners<T> stateListeners;
     private final Map<XyzState, Set<Consumer<T>>> callbackCollection;
     private final Consumer<T> callback;
 
-    public XyzStateListenerRegistration(XyzStateListeners<T> xyzStateListeners,
+    public XyzStateListenerRegistration(XyzStateListeners<T> stateListeners,
                                         Map<XyzState, Set<Consumer<T>>> callbackCollection,
                                         Consumer<T> callback) {
-        this.xyzStateListeners = xyzStateListeners;
+        this.stateListeners = stateListeners;
         this.callbackCollection = callbackCollection;
         this.callback = callback;
     }
 
     void detach() {
-        xyzStateListeners.removeListener(callbackCollection, callback);
+        stateListeners.removeListener(callbackCollection, callback);
     }
 }
